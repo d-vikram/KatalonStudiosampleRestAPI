d0c8027489f9f5ad6a1670b2ea73610d6a5f8e7a
UMA <UMA@LAPTOP-PTOLBIN0>
2019-03-19 16:25:29 +0530

    second commit

---
+++ Test Suites/Test1.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>b285d429-9fba-4906-bdb2-e1b2c24eef42</testSuiteGuid>
+   <testCaseLink>
+      <guid>81a1be6e-6787-4736-b48e-208270874b8d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TesthhtpMethods</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>629d6a12-9bfb-4c4e-9346-1b51d3612c98</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/sampletestone</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4f450678-8d3a-4def-a4a7-1f0621b6fcf9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ListUsersTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2c01d1d9-ebe8-4841-a10d-0ccf8c84b564</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GetBankDetails</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
